d07a09d9592657dd47356488a045be72460c8da4
Dawid Ciepko <dciepko17@gmail.com>
2024-04-09 17:46:32 +0200

    Adding placeholder buttons to the project header section

--- project-management-project/frontend/src/components/SelectedProject/SelectedProject.jsx
+++ project-management-project/frontend/src/components/SelectedProject/SelectedProject.jsx
@@ -6,6 +6,13 @@ export default function SelectedProject({ currentProject }) {
     <div className={classes.container}>
       <header className={classes.header}>
         <h1 className={classes.h1}>{currentProject.title}</h1>
+        <button className={classes.unwrapButton}>V</button>
+        <section className={classes.optionButtonsContainer}>
+          <button className={classes.optionButton}>Zmiana widoku</button>
+          <button className={classes.optionButton}>Filtry</button>
+          <button className={classes.optionButton}>Uczestnicy</button>
+          <button className={classes.optionButton}>Dodaj</button>
+        </section>
       </header>
       <div className={classes.tableContainer}>
         <TimeTable title={"Do zrobienia"} tasks={currentProject.tasks} />
