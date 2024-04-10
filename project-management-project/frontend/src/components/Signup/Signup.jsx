7efaeeec7c9def5f540dc00cd8f68962b65af39e
Dawid Ciepko <dciepko17@gmail.com>
2024-04-10 21:47:03 +0200

    Implementing validation in the Signup component

--- project-management-project/frontend/src/components/Signup/Signup.jsx
+++ project-management-project/frontend/src/components/Signup/Signup.jsx
@@ -1,22 +1,127 @@
+import {
+  hasMinLength,
+  isNotEmpty,
+  isEmail,
+} from "../../util/validationFunctions";
 import Input from "../Input/Input";
+
+import { useInput } from "../../hooks/useInput";
 
 import classes from "./Signup.module.css";
 
 export default function Signup() {
+  const {
+    value: firstNameValue,
+    handleInputChange: handleFirstNameChange,
+    handleInputBlur: handleFirstNameBlur,
+    hasError: firstNameHasError,
+  } = useInput("", (value) => isNotEmpty(value));
+  const {
+    value: lastNameValue,
+    handleInputChange: handleLastNameChange,
+    handleInputBlur: handleLastNameBlur,
+    hasError: lastNameHasError,
+  } = useInput("", (value) => isNotEmpty(value));
+  const {
+    value: emailValue,
+    handleInputChange: handleEmailChange,
+    handleInputBlur: handleEmailBlur,
+    hasError: emailHasError,
+  } = useInput("", (value) => isEmail(value) && isNotEmpty(value));
+  const {
+    value: nickValue,
+    handleInputChange: handleNickChange,
+    handleInputBlur: handleNickBlur,
+    hasError: nickHasError,
+  } = useInput("", (value) => isNotEmpty(value));
+  const {
+    value: passwordValue,
+    handleInputChange: handlePasswordChange,
+    handleInputBlur: handlePasswordBlur,
+    hasError: passwordHasError,
+  } = useInput("", (value) => isNotEmpty(value) && hasMinLength(value, 6));
+  const {
+    value: passwordRepeatValue,
+    handleInputChange: handlePasswordRepeatChange,
+    handleInputBlur: handlePasswordRepeatBlur,
+    hasError: passwordRepeatHasError,
+  } = useInput("", (value) => isNotEmpty(value) && hasMinLength(value, 6));
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(
+      firstNameValue,
+      lastNameValue,
+      emailValue,
+      nickValue,
+      passwordValue,
+      passwordRepeatValue
+    );
+  }
+
   return (
-    <form className={classes.formContainer}>
+    <form onSubmit={handleSubmit} className={classes.formContainer}>
       <h2 className={classes.h2}>Zarejestruj się</h2>
       <div className={classes.inputsContainer}>
-        <Input label="Imię" id="firstName" type="text" name="firstName" />
-        <Input label="Nazwisko" id="lastName" type="text" name="lastName" />
-        <Input label="Email" id="email" type="email" name="email" />
-        <Input label="Nazwa użytkownika" id="nick" type="text" name="nick" />
-        <Input label="Hasło" id="password" type="password" name="password" />
+        <Input
+          label="Imię"
+          id="firstName"
+          type="text"
+          name="firstName"
+          onBlur={handleFirstNameBlur}
+          onChange={handleFirstNameChange}
+          value={firstNameValue}
+          error={firstNameHasError && "Wprowadź porawne imię"}
+        />
+        <Input
+          label="Nazwisko"
+          id="lastName"
+          type="text"
+          name="lastName"
+          onBlur={handleLastNameBlur}
+          onChange={handleLastNameChange}
+          value={lastNameValue}
+          error={lastNameHasError && "Wprowadź porawne nazwisko"}
+        />
+        <Input
+          label="Email"
+          id="email"
+          type="email"
+          name="email"
+          onBlur={handleEmailBlur}
+          onChange={handleEmailChange}
+          value={emailValue}
+          error={emailHasError && "Wprowadź porawny adres email"}
+        />
+        <Input
+          label="Nazwa użytkownika"
+          id="nick"
+          type="text"
+          name="nick"
+          onBlur={handleNickBlur}
+          onChange={handleNickChange}
+          value={nickValue}
+          error={nickHasError && "Wprowadź porawny nick"}
+        />
+        <Input
+          label="Hasło"
+          id="password"
+          type="password"
+          name="password"
+          onBlur={handlePasswordBlur}
+          onChange={handlePasswordChange}
+          value={passwordValue}
+          error={passwordHasError && "Wprowadź porawne hasło"}
+        />
         <Input
           label="Powtórz hasło"
           id="passwordRepeat"
           type="password"
           name="passwordRepeat"
+          onBlur={handlePasswordRepeatBlur}
+          onChange={handlePasswordRepeatChange}
+          value={passwordRepeatValue}
+          error={passwordRepeatHasError && "Wprowadź porawne hasło"}
         />
         <div>
           <button className={classes.formButton}>Reset</button>
