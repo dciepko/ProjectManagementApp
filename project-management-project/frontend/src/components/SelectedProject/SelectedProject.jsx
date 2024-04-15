98f0ba248f0df111b420b07562f6f903464063a6
olaflolaf <olafko25@wp.pl>
2024-04-15 21:33:08 +0200

    Styling SelectedProject component

--- project-management-project/frontend/src/components/SelectedProject/SelectedProject.jsx
+++ project-management-project/frontend/src/components/SelectedProject/SelectedProject.jsx
@@ -1,17 +1,26 @@
 import StatusTable from "../StatusTable/StatusTable";
 import classes from "./SelectedProject.module.css";
 
+import filters from "../../assets/filters-icon.png";
+import group from "../../assets/group-icon.png";
+import plus from "../../assets/plus-icon.png";
+import wrap from "../../assets/wrap-down-button.png";
+
 export default function SelectedProject({ currentProject }) {
   return (
     <div className={classes.container}>
       <header className={classes.header}>
         <h1 className={classes.h1}>{currentProject.title}</h1>
-        <button className={classes.unwrapButton}>V</button>
+        <button className={classes.unwrapButton}><img src={wrap} alt="wrap header" /></button>
         <section className={classes.optionButtonsContainer}>
-          <button className={classes.optionButton}>Zmiana widoku</button>
-          <button className={classes.optionButton}>Filtry</button>
-          <button className={classes.optionButton}>Uczestnicy</button>
-          <button className={classes.optionButton}>Dodaj</button>
+          <select className={classes.selectButton}>
+            <option  value="classic">Tablica</option>
+            <option  value="wide">Podłużny</option>
+            <option  value="table">Tabela</option>
+            </select>
+          <button className={classes.optionButton}><img src={filters} alt="filter button" /></button>
+          <button className={classes.optionButton}><img src={group} alt="group button" /></button>
+          <button className={classes.optionButton}><img src={plus} alt="add button" /></button>
         </section>
       </header>
       <div className={classes.tableContainer}>
