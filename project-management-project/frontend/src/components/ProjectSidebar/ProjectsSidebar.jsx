f51dc100b37fed306bf706cfebf5e1dcd47538f1
Dawid Ciepko <dciepko17@gmail.com>
2024-04-15 20:31:42 +0200

    Styling navigation bar

--- project-management-project/frontend/src/components/ProjectSidebar/ProjectsSidebar.jsx
+++ project-management-project/frontend/src/components/ProjectSidebar/ProjectsSidebar.jsx
@@ -1,11 +1,15 @@
 import classes from "./ProjectSidebar.module.css";
 
+import wrapButton from "../../assets/wrap-right-button.png";
+
 export default function ProjectsSidebar({ projectList, handleClick }) {
   return (
     <aside className={classes.aside}>
       <header className={classes.asideHeader}>
         <h2 className={classes.h2}>Twoje Projekty</h2>
-        <button className={classes.wrapButton}>&lt;</button>
+        <button className={classes.wrapButton}>
+          <img src={wrapButton} alt="Wrap button" />
+        </button>
       </header>
 
       <ul className={classes.ul}>
