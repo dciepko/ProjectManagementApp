6f421a2aba91c46d32c7812065f6c217a372e149
Dawid Ciepko <dciepko17@gmail.com>
2024-04-17 14:31:27 +0200

    Creating User Controller

---
+++ project-management-project/backend/Project-Management-App/src/main/java/com/pma/ProjectManagementApp/controllers/UserController.java
@@ -0,0 +1,34 @@
+package com.pma.ProjectManagementApp.controllers;
+
+import com.pma.ProjectManagementApp.modules.User;
+import com.pma.ProjectManagementApp.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return service.getUsers();
+    }
+
+    @PostMapping("/users")
+    public void addUser(@RequestBody User newUser) {
+        service.addUser(newUser);
+    }
+
+    @PutMapping("/users")
+    public void editUser(@RequestParam Integer id, @RequestBody User newUser) {
+        service.editUser(id, newUser);
+    }
+
+    @DeleteMapping("/users")
+    public void deleteUser(@RequestParam Integer id) {
+        service.deleteUser(id);
+    }
+}
