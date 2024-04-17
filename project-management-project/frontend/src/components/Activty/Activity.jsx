880be318e10f6b56c3099f30d7bb9cf0f953d552
Dawid Ciepko <dciepko17@gmail.com>
2024-04-17 21:33:00 +0200

    Creating the wrapped activity view

--- project-management-project/frontend/src/components/Activty/Activity.jsx
+++ project-management-project/frontend/src/components/Activty/Activity.jsx
@@ -1,17 +1,53 @@
 import classes from "./Activity.module.css";
 
+import lupa from "../../assets/find-icon.svg";
+import wrap from "../../assets/wrap-down-button.png";
+
 import DropIndicator from "../DropIndicator/DropIndicator";
+import { useState } from "react";
 
 export default function Activity({ id, task, handleDragStart }) {
+  const [viewType, setViewType] = useState("wrapped");
+  console.log(task.type);
+
   return (
     <>
-      <DropIndicator table={id} beforeId={task.id} />
+      {/* <DropIndicator table={id} beforeId={task.id} /> */}
+      {/* {viewType === "wrapped" && (
+        <div
+          draggable
+          className={classes.activityContainer}
+          onDragStart={(event) => handleDragStart(event, task)}
+        >
+          <div>
+            <p>{task.title}</p>
+            {task.type == "task" && <progress max={100} value={70} />}
+          </div>
+          <div className={classes.buttonContainer}>
+            <button className={classes.button}>O</button>
+            <button className={classes.button}>
+              <img src={wrap} alt="wrap button" />
+            </button>
+          </div>
+        </div>
+      )} */}
+
       <div
         draggable
         className={classes.activityContainer}
         onDragStart={(event) => handleDragStart(event, task)}
       >
-        {task.title}
+        <div>
+          <p>{task.title}</p>
+          {task.type == "task" && <progress max={100} value={70} />}
+          {task.type == "task" && <div>{task.checklist}</div>}
+        </div>
+        <div className={classes.buttonContainer}>
+          <button className={classes.button}>O</button>
+          <button className={classes.button}>
+            <img src={wrap} alt="wrap button" />
+          </button>
+        </div>
       </div>
     </>
   );
