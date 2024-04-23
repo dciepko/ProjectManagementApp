e00570961736b7ecb8022108e219d52adfb3cdbd
olaflolaf <olafko25@wp.pl>
2024-04-23 20:29:09 +0200

    Styling LongBoard View

--- project-management-project/frontend/src/components/ProjectLayout/StatusList/StatusList.jsx
+++ project-management-project/frontend/src/components/ProjectLayout/StatusList/StatusList.jsx
@@ -1,4 +1,10 @@
 import classes from "./StatusList.module.css";
+import arrow from "../../../assets/arrow-down-icon.png";
+import info from "../../../assets/info-icon.png";
+import change from "../../../assets/switch-icon.png";
+import wrap from "../../../assets/wrap-down2.png";
+import user1 from "../../../assets/user-placeholder.png";
+import user2 from "../../../assets/user-placeholder2.png";
 
 export default function StatusList({
   activities,
@@ -13,7 +19,11 @@ export default function StatusList({
     <section>
       <div className={classes.statusLine}>
         <div>{title}</div>
-        <div>Przycisk</div>
+        <div>
+          <button className={classes.wrapButton}>
+          <img src={wrap} alt="wrap button" />
+        </button>
+        </div>
         <div>Priorytet</div>
         <div>Uczestnicy</div>
         <div>Data</div>
@@ -23,9 +33,23 @@ export default function StatusList({
         return (
           <div className={classes.activityLine}>
             <div>{activity.title}</div>
-            <div>przyciski</div>
+            <div>
+        <button className={classes.optionButton}>
+          <img src={info} alt="info button" />
+        </button>
+        <button className={classes.optionButton}>
+          <img src={arrow} alt="arrow button" />
+        </button>
+        <button className={classes.optionButton}>
+          <img src={change} alt="switch button" />
+        </button>
+        </div>
             <div>Priorytet</div>
-            <div>Uczestnicy</div>
+            <div>
+            <button className={classes.avatarImage}>
+          <img src={user1} alt="user-avatar" />
+        </button>
+              </div>
             <div>Data</div>
           </div>
         );
