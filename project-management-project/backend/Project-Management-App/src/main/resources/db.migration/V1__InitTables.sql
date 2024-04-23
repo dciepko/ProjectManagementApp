7dc7480de8a7cf9fb99dd436842151dcba8e4cc9
Agata Bialy <115097312+Agatauo@users.noreply.github.com>
2024-04-23 21:52:05 +0200

    Adding data for the Avatar entity in the sql file

--- project-management-project/backend/Project-Management-App/src/main/resources/db.migration/V1__InitTables.sql
+++ project-management-project/backend/Project-Management-App/src/main/resources/db.migration/V1__InitTables.sql
@@ -2,10 +2,14 @@ INSERT INTO user (userid, user_name, user_surename, user_nickname, user_email, u
 INSERT INTO user (userid, user_name, user_surename, user_nickname, user_email, user_password, working_hours, is_owner) VALUES (2, 'Franciszek', 'Adamski', 'Adams', 'adams@example.com', 'password123', '8-16', FALSE);
 INSERT INTO user (userid, user_name, user_surename, user_nickname, user_email, user_password, working_hours, is_owner) VALUES (3, 'Gabriel', 'Wilk', 'Wilkor', 'gwilk@example.com', 'password123', '6-16', FALSE);
 
-INSERT INTO activity (activityid, activity_description, activity_name, activity_priority, activity_type, due_date, statusid, labelid, tableid) VALUES(1, 'meeting1', 'meeting', 1, 1, '21.03.2024', 1, 1, 1);
-INSERT INTO activity (activityid, activity_description, activity_name, activity_priority, activity_type, due_date, statusid, labelid, tableid) VALUES(2, 'task1', 'task', 1, 1, '22.03.2024', 2, 2, 2);
-INSERT INTO activity (activityid, activity_description, activity_name, activity_priority, activity_type, due_date, statusid, labelid, tableid) VALUES(3, 'milestone1', 'milestone', 1, 1, '23.03.2024', 3, 3, 3);
+INSERT INTO activity (activityid, activity_description, activity_name, activity_priority, activity_type, due_date, statusid, labelid, tableid) VALUES (1, 'meeting1', 'meeting', 1, 1, '21.03.2024', 1, 1, 1);
+INSERT INTO activity (activityid, activity_description, activity_name, activity_priority, activity_type, due_date, statusid, labelid, tableid) VALUES (2, 'task1', 'task', 1, 1, '22.03.2024', 2, 2, 2);
+INSERT INTO activity (activityid, activity_description, activity_name, activity_priority, activity_type, due_date, statusid, labelid, tableid) VALUES (3, 'milestone1', 'milestone', 1, 1, '23.03.2024', 3, 3, 3);
 
-INSERT INTO attachement (attachementid, attachement_name, attachement_type, attachement_url, date, activityid, userid) VALUES(1, 'zalacznik1', 'link', 'example.com', '21.03.2024', 1, 1);
-INSERT INTO attachement (attachementid, attachement_name, attachement_type, attachement_url, date, activityid, userid) VALUES(1, 'zalacznik2', 'link', 'example.com', '22.03.2024', 2, 2);
-INSERT INTO attachement (attachementid, attachement_name, attachement_type, attachement_url, date, activityid, userid) VALUES(1, 'zalacznik3', 'link', 'example.com', '23.03.2024', 3, 3);
+INSERT INTO attachement (attachementid, attachement_name, attachement_type, attachement_url, date, activityid, userid) VALUES (1, 'zalacznik1', 'link', 'example.com', '21.03.2024', 1, 1);
+INSERT INTO attachement (attachementid, attachement_name, attachement_type, attachement_url, date, activityid, userid) VALUES (2, 'zalacznik2', 'link', 'example.com', '22.03.2024', 2, 2);
+INSERT INTO attachement (attachementid, attachement_name, attachement_type, attachement_url, date, activityid, userid) VALUES (3, 'zalacznik3', 'link', 'example.com', '23.03.2024', 3, 3);
+
+INSERT INTO avatar (avatarid, avatar_url, userid) VALUES (1, "exampleurl.com", 1);
+INSERT INTO avatar (avatarid, avatar_url, userid) VALUES (2, "exampleurl.com", 2);
+INSERT INTO avatar (avatarid, avatar_url, userid) VALUES (3, "exampleurl.com", 3);
