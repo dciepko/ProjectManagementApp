d5d9b30c6c4e92fd7d456c2edfdb6f83d6ce2914
Dawid Ciepko <dciepko17@gmail.com>
2024-04-23 11:03:21 +0200

    Implementing board project view

--- project-management-project/frontend/src/components/ProjectLayout/ProjectBoards/ProjectBoardGroup/ProjectBoardGroup.jsx
+++ project-management-project/frontend/src/components/ProjectLayout/ProjectBoards/ProjectBoardGroup/ProjectBoardGroup.jsx
@@ -1,3 +1,44 @@
+import classes from "./ProjectBoardGroup.module.css";
+
 export default function ProjectBoardGroup() {
-  return <p>Grupowanie</p>;
+  return (
+    <div className={classes.gruopContainer}>
+      <button
+        className={classes.groupButton}
+        style={{ backgroundColor: "red" }}
+      >
+        Wysoki Priorytet
+      </button>
+      <button
+        className={classes.groupButton}
+        style={{ backgroundColor: "#44BA26" }}
+      >
+        Na ten tydzień
+      </button>
+      <button
+        className={classes.groupButton}
+        style={{ backgroundColor: "#D4AF51" }}
+      >
+        W trakcie
+      </button>
+      <button
+        className={classes.groupButton}
+        style={{ backgroundColor: "orange" }}
+      >
+        Twoje zadania
+      </button>
+      <button
+        className={classes.groupButton}
+        style={{ backgroundColor: "purple" }}
+      >
+        Meetingi
+      </button>
+      <button
+        className={classes.groupButton}
+        style={{ backgroundColor: "gray" }}
+      >
+        +
+      </button>
+    </div>
+  );
 }
