a1024c3eadfb789f499a780aab995b6805ccece3
Dawid Ciepko <dciepko17@gmail.com>
2024-04-25 14:46:36 +0200

    Implementing global CORS configuration

--- project-management-project/backend/Project-Management-App/src/main/java/com/pma/ProjectManagementApp/ProjectManagementAppApplication.java
+++ project-management-project/backend/Project-Management-App/src/main/java/com/pma/ProjectManagementApp/ProjectManagementAppApplication.java
@@ -2,6 +2,9 @@ package com.pma.ProjectManagementApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProjectManagementAppApplication {
@@ -10,4 +13,15 @@ public class ProjectManagementAppApplication {
 		SpringApplication.run(ProjectManagementAppApplication.class, args);
 	}
 
+	@Configuration
+	public class WebConfig implements WebMvcConfigurer {
+
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**")
+					.allowedOrigins("http://localhost:5173")
+					.allowedMethods("GET", "POST", "PUT", "DELETE")
+					.allowedHeaders("*");
+		}
+	}
 }
