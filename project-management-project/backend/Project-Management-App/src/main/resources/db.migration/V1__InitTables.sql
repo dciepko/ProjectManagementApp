89c81305a3c31028c71027959a65347d3a0e286c
Agata Bialy <agata3272000@gmail.com>
2024-05-06 18:43:40 +0200

    Adding data

--- project-management-project/backend/Project-Management-App/src/main/resources/db.migration/V1__InitTables.sql
+++ project-management-project/backend/Project-Management-App/src/main/resources/db.migration/V1__InitTables.sql
@@ -2,9 +2,10 @@
  INSERT INTO user (userid, user_name, user_surename, user_nickname, user_email, user_password, working_hours, is_owner) VALUES (2, 'Franciszek', 'Adamski', 'Adams', 'adams@example.com', 'password123', '8-16', TRUE);
  INSERT INTO user (userid, user_name, user_surename, user_nickname, user_email, user_password, working_hours, is_owner) VALUES (3, 'Gabriel', 'Wilk', 'Wilkor', 'gwilk@example.com', 'password123', '6-16', FALSE);
 
- INSERT INTO status (statusid, status_name) VALUES (1, 'Do zrobienia');
- INSERT INTO status (statusid, status_name) VALUES (2, 'W trakcie');
- INSERT INTO status (statusid, status_name) VALUES (3, 'Zakonczone');
+ INSERT INTO status (statusid, status_name) VALUES (1, 'To do');
+ INSERT INTO status (statusid, status_name) VALUES (2, 'In progress');
+ INSERT INTO status (statusid, status_name) VALUES (3, 'Testing');
+ INSERT INTO status (statusid, status_name) VALUES (4, 'Ended');
 
  INSERT INTO label (labelid, label_color) VALUES(1, 'green');
  INSERT INTO label (labelid, label_color) VALUES(2, 'blue');
@@ -14,13 +15,20 @@
  INSERT INTO status_table (tableid, table_color, table_name) VALUES (2, 'purple', 'table2');
  INSERT INTO status_table (tableid, table_color, table_name) VALUES (3, 'green', 'table3');
 
- INSERT INTO activity (activityid, activity_description, activity_name, activity_priority, activity_type, due_date, statusid, labelid, tableid) VALUES (1, 'meeting1', 'meeting', 1, 1, '2024-03-21', 1, 1, 1);
- INSERT INTO activity (activityid, activity_description, activity_name, activity_priority, activity_type, due_date, statusid, labelid, tableid) VALUES (2, 'task1', 'task', 1, 1, '2024-03-21', 2, 2, 2);
- INSERT INTO activity (activityid, activity_description, activity_name, activity_priority, activity_type, due_date, statusid, labelid, tableid) VALUES (3, 'milestone1', 'milestone', 1, 1, '2024-03-21', 3, 3, 3);
+ INSERT INTO activity (activityid, activity_description, activity_name, activity_priority, activity_type, due_date, statusid, labelid, tableid) VALUES (1, 'Summary of this week s achievements', 'Weekly meeting', 1, 1, '2024-03-21', 1, 1, 1);
+ INSERT INTO activity (activityid, activity_description, activity_name, activity_priority, activity_type, due_date, statusid, labelid, tableid) VALUES (2, 'Discussion of code errors', 'Errors meeting', 1, 1, '2024-03-21', 1, 1, 1);
+ INSERT INTO activity (activityid, activity_description, activity_name, activity_priority, activity_type, due_date, statusid, labelid, tableid) VALUES (3, 'Discussion on marketing strategy', 'Marketing meeting', 1, 1, '2024-03-21', 1, 1, 1);
+ INSERT INTO activity (activityid, activity_description, activity_name, activity_priority, activity_type, due_date, statusid, labelid, tableid) VALUES (4, 'Designing the appearance of the home page', 'Home page design', 1, 2, '2024-03-21', 2, 2, 1);
+ INSERT INTO activity (activityid, activity_description, activity_name, activity_priority, activity_type, due_date, statusid, labelid, tableid) VALUES (5, 'Add a post to the home page', 'Adding post', 1, 2, '2024-03-21', 3, 3, 1);
+ INSERT INTO activity (activityid, activity_description, activity_name, activity_priority, activity_type, due_date, statusid, labelid, tableid) VALUES (6, 'Improve the code in the likeunlike method', 'Code improvement', 1, 2, '2024-03-22', 3, 3, 1);
+ INSERT INTO activity (activityid, activity_description, activity_name, activity_priority, activity_type, due_date, statusid, labelid, tableid) VALUES (7, 'Completing the mobile version of the application', 'Mobile version', 1, 3, '2024-03-21', 3, 3, 1);
+ INSERT INTO activity (activityid, activity_description, activity_name, activity_priority, activity_type, due_date, statusid, labelid, tableid) VALUES (8, 'Completing the design of the application s appearance in the mobile and desktop versions', 'Completed design', 1, 3, '2024-03-21', 3, 3, 1);
+ INSERT INTO activity (activityid, activity_description, activity_name, activity_priority, activity_type, due_date, statusid, labelid, tableid) VALUES (9, 'Reaching 10,000 subscribers', 'Subscribe goal', 1, 3, '2024-03-28', 3, 3, 1);
 
- INSERT INTO attachement (attachementid, attachement_name, attachement_type, attachement_url, date, activityid, userid) VALUES (1, 'zalacznik1', 'link', 'example.com', '2024-03-21', 1, 1);
- INSERT INTO attachement (attachementid, attachement_name, attachement_type, attachement_url, date, activityid, userid) VALUES (2, 'zalacznik2', 'link', 'example.com', '2024-03-21', 2, 2);
- INSERT INTO attachement (attachementid, attachement_name, attachement_type, attachement_url, date, activityid, userid) VALUES (3, 'zalacznik3', 'link', 'example.com', '2024-03-21', 3, 3);
+
+ INSERT INTO attachement (attachementid, attachement_name, attachement_type, attachement_url, date, activityid, userid) VALUES (1, 'Instructional video', 'url', 'example.com', '2024-03-21', 1, 1);
+ INSERT INTO attachement (attachementid, attachement_name, attachement_type, attachement_url, date, activityid, userid) VALUES (2, 'Database project', 'url', 'example.com', '2024-03-21', 2, 2);
+ INSERT INTO attachement (attachementid, attachement_name, attachement_type, attachement_url, date, activityid, userid) VALUES (3, 'Ideas for icons', 'url', 'example.com', '2024-03-21', 3, 3);
 
  INSERT INTO avatar (avatarid, avatar_url, userid) VALUES (1, 'exampleurl.com', 1);
  INSERT INTO avatar (avatarid, avatar_url, userid) VALUES (2, 'exampleurl.com', 2);
@@ -30,33 +38,33 @@
  INSERT INTO checklist (checklistid, is_done) VALUES (2, FALSE);
  INSERT INTO checklist (checklistid, is_done) VALUES (3, FALSE);
 
- INSERT INTO checklist_element (checklist_elementid, element_name, is_done, checklistid) VALUES (1, 'element 1', FALSE, 1);
- INSERT INTO checklist_element (checklist_elementid, element_name, is_done, checklistid) VALUES (2, 'element 2', FALSE, 2);
- INSERT INTO checklist_element (checklist_elementid, element_name, is_done, checklistid) VALUES (3, 'element 3', FALSE, 3);
+ INSERT INTO checklist_element (checklist_elementid, element_name, is_done, checklistid) VALUES (1, 'create new project', FALSE, 1);
+ INSERT INTO checklist_element (checklist_elementid, element_name, is_done, checklistid) VALUES (2, 'add all objects', FALSE, 2);
+ INSERT INTO checklist_element (checklist_elementid, element_name, is_done, checklistid) VALUES (3, 'create methods', FALSE, 2);
 
- INSERT INTO comment (commentid, comment, comment_date, activityid, userid) VALUES (1, 'komentarz 1', '2024-03-21', 1, 1);
- INSERT INTO comment (commentid, comment, comment_date, activityid, userid) VALUES (2, 'komentarz 2', '2024-03-21', 2, 1);
- INSERT INTO comment (commentid, comment, comment_date, activityid, userid) VALUES (3, 'komentarz 3', '2024-03-21', 3, 1);
+ INSERT INTO comment (commentid, comment, comment_date, activityid, userid) VALUES (1, 'Change the icon to a heart', '2024-03-21', 1, 1);
+ INSERT INTO comment (commentid, comment, comment_date, activityid, userid) VALUES (2, 'Incorrectly constructed method', '2024-03-21', 1, 1);
+ INSERT INTO comment (commentid, comment, comment_date, activityid, userid) VALUES (3, 'Great job!', '2024-03-25', 1, 2);
 
  INSERT INTO meeting (meetingid, pred_dur_min, activityid) VALUES (1, 20, 1);
- INSERT INTO meeting (meetingid, pred_dur_min, activityid) VALUES (2, 20, 2);
- INSERT INTO meeting (meetingid, pred_dur_min, activityid) VALUES (3, 20, 3);
+-- INSERT INTO meeting (meetingid, pred_dur_min, activityid) VALUES (2, 20, 2);
+-- INSERT INTO meeting (meetingid, pred_dur_min, activityid) VALUES (3, 20, 3);
 
  INSERT INTO milestone (milestoneid, actual_date, activityid) VALUES(1, '2024-03-21', 1);
- INSERT INTO milestone (milestoneid, actual_date, activityid) VALUES(2, '2024-03-21', 2);
- INSERT INTO milestone (milestoneid, actual_date, activityid) VALUES(3, '2024-03-21', 3);
+-- INSERT INTO milestone (milestoneid, actual_date, activityid) VALUES(2, '2024-03-21', 2);
+-- INSERT INTO milestone (milestoneid, actual_date, activityid) VALUES(3, '2024-03-21', 3);
 
- INSERT INTO team (teamid, team_name) VALUES (1, 'zespol programistyczny');
- INSERT INTO team (teamid, team_name) VALUES (2, 'zespol designu');
- INSERT INTO team (teamid, team_name) VALUES (3, 'zespol managerski');
+ INSERT INTO team (teamid, team_name) VALUES (1, 'Programming team');
+ INSERT INTO team (teamid, team_name) VALUES (2, 'Design team');
+ INSERT INTO team (teamid, team_name) VALUES (3, 'Managers team');
 
- INSERT INTO project (projectid, end_date, ownerid, project_description, project_name, start_date, statusid, tableid, teamid) VALUES(1, '2024-03-21', 1, 'description 1', 'Projekt1', '2024-03-21', 1, 1, 1);
- INSERT INTO project (projectid, end_date, ownerid, project_description, project_name, start_date, statusid, tableid, teamid) VALUES(2, '2024-03-21', 1, 'description 2', 'Projekt2', '2024-03-21', 2, 2, 2);
- INSERT INTO project (projectid, end_date, ownerid, project_description, project_name, start_date, statusid, tableid, teamid) VALUES(3, '2024-03-21', 1, 'description 3', 'Projekt3', '2024-03-21', 3, 3, 3);
+ INSERT INTO project (projectid, end_date, ownerid, project_description, project_name, start_date, statusid, tableid, teamid) VALUES(1, '2024-03-21', 1, 'Creating a database for a secret project', 'Database Project', '2024-03-21', 1, 1, 1);
+ INSERT INTO project (projectid, end_date, ownerid, project_description, project_name, start_date, statusid, tableid, teamid) VALUES(2, '2024-03-21', 1, 'Application design for creating databases', 'DatabasesApp', '2024-03-21', 2, 2, 2);
+ INSERT INTO project (projectid, end_date, ownerid, project_description, project_name, start_date, statusid, tableid, teamid) VALUES(3, '2024-03-21', 1, 'Creating a dog training application', 'DogTrainingApp', '2024-03-21', 3, 3, 3);
 
- INSERT INTO task (taskid, activityid, chechlistid) VALUES (1, 1, 1);
+-- INSERT INTO task (taskid, activityid, chechlistid) VALUES (1, 1, 1);
  INSERT INTO task (taskid, activityid, chechlistid) VALUES (1, 2, 2);
- INSERT INTO task (taskid, activityid, chechlistid) VALUES (1, 3, 3);
+-- INSERT INTO task (taskid, activityid, chechlistid) VALUES (1, 3, 3);
 
  INSERT INTO activity_project (activityid, projectid) VALUES (1, 1);
  INSERT INTO activity_project (activityid, projectid) VALUES (2, 2);
