2ea6659102f92c9b038a568bd15a4d80571c8a64
olaflolaf <olafko25@wp.pl>
2024-05-07 21:30:14 +0200

    Styling HomeActivityElement component

--- project-management-project/frontend/src/components/ActivityHomeElement/ActivityHomeElement.jsx
+++ project-management-project/frontend/src/components/ActivityHomeElement/ActivityHomeElement.jsx
@@ -1,11 +1,13 @@
+import classes from "./ActivityHomeElement.module.css";
+
 export default function ActivityHomeElement() {
   return (
-    <div>
-      <div>
-        <div>Task1</div>
-        <p>Projekt 1: Do Zrobienia</p>
+    <div className={classes.container}>
+      <div className={classes.mainPart}>
+        <div className={classes.namePart}>Task1</div>
+        <div className={classes.mainP}>Projekt 1: Do Zrobienia</div>
       </div>
-      <div>
+      <div className={classes.descriptionPart}>
         <p>Dodano Cię do aktywności!</p>
       </div>
     </div>
