fff222f255c8dd882b933421de1a4c87334c34fe
Dawid Ciepko <dciepko17@gmail.com>
2024-06-17 10:31:13 +0200

    Implementing statusBoard deletion

--- project-management-project/backend/Project-Management-App/src/main/java/com/pma/ProjectManagementApp/repos/ActivityRepo.java
+++ project-management-project/backend/Project-Management-App/src/main/java/com/pma/ProjectManagementApp/repos/ActivityRepo.java
@@ -1,6 +1,7 @@
 package com.pma.ProjectManagementApp.repos;
 
 import com.pma.ProjectManagementApp.modules.Activity;
+import com.pma.ProjectManagementApp.modules.StatusTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,5 @@ import java.util.List;
 @Repository
 public interface ActivityRepo extends JpaRepository<Activity, Integer> {
     List<Activity> findByActivityProjectProjectID(Integer id);
+    void deleteByTableA(StatusTable statusTable);
 }
--- project-management-project/backend/Project-Management-App/src/main/java/com/pma/ProjectManagementApp/services/StatusTableService.java
+++ project-management-project/backend/Project-Management-App/src/main/java/com/pma/ProjectManagementApp/services/StatusTableService.java
@@ -1,6 +1,9 @@
 package com.pma.ProjectManagementApp.services;
 
+import com.pma.ProjectManagementApp.modules.Project;
 import com.pma.ProjectManagementApp.modules.StatusTable;
+import com.pma.ProjectManagementApp.repos.ActivityRepo;
+import com.pma.ProjectManagementApp.repos.ProjectRepo;
 import com.pma.ProjectManagementApp.repos.StatusTableRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +12,10 @@ import org.springframework.stereotype.Service;
 public class StatusTableService {
     @Autowired
     StatusTableRepo repo;
+    @Autowired
+    ProjectRepo projectRepo;
+    @Autowired
+    ActivityRepo activityRepo;
 
     public void editStatusTable(Integer id, StatusTable newStatusTable) {
         StatusTable editedStatusTable = repo.findById(id).get();
@@ -17,6 +24,20 @@ public class StatusTableService {
             editedStatusTable.setTableColor(newStatusTable.getTableColor());
             repo.save(editedStatusTable);
         }
+    }
+
+    public void deleteStatusTableById(Integer tableID) {
+        StatusTable statusTable = repo.findById(tableID).orElseThrow(() -> new IllegalArgumentException("StatusTable with ID " + tableID + " does not exist"));
+
+        activityRepo.deleteByTableA(statusTable);  // usuwam aktywnosci bo nie beda potrzebne jak nie ma boarda do ktorego byly przypisane
+
+        Project project = statusTable.getProject();  // nie usuwam projektu ale biore z jego listy tablic te konkretna tablice i ja usuwam
+        if (project != null) {
+            project.getTables().remove(statusTable);
+            projectRepo.save(project);
+        }
+
+        repo.deleteById(tableID);
     }
 
     public void deleteUser(Integer id) {
