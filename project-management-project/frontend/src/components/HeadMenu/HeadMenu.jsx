d435cc1aaa89f311aab381e179084a8e9cfdd6c0
Dawid Ciepko <dciepko17@gmail.com>
2024-06-26 23:42:20 +0200

    Implementing quick creation

--- project-management-project/frontend/src/components/HeadMenu/HeadMenu.jsx
+++ project-management-project/frontend/src/components/HeadMenu/HeadMenu.jsx
@@ -15,11 +15,19 @@ import { getTokenDuration } from "../../util/auth";
 import { useEffect, useState, useRef } from "react";
 import { useMediaQuery } from "react-responsive";
 
+import AddingTask from "../Modals/AddingTask/AddingTask";
+import AddingProject from "../Modals/AddingProject/AddingProject";
+import AddingWorkspace from "../Modals/AddingWorkspace/AddingWorkspace";
+
 export default function HeadMenu() {
-  const isMobileScreen = useMediaQuery({ query: "(max-width:320px)" });
+  const isMobileScreen = useMediaQuery({ query: "(max-width: 320px)" });
   const navigate = useNavigate();
   const token = useLoaderData();
   const submit = useSubmit();
+
+  const taskRef = useRef();
+  const projectRef = useRef();
+  const workspaceRef = useRef();
 
   const currentUser = localStorage.getItem("currentUserID");
 
@@ -29,6 +37,11 @@ export default function HeadMenu() {
     left: 0,
   });
   const buttonRef = useRef(null);
+
+  const [isCreateMenuOpen, setIsCreateMenuOpen] = useState(false);
+  const toggleCreateMenu = () => {
+    setIsCreateMenuOpen(!isCreateMenuOpen);
+  };
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -66,8 +79,23 @@ export default function HeadMenu() {
     navigate("/");
   }
 
+  function handleOpenProject() {
+    projectRef.current.open();
+  }
+
+  function handleOpenTask() {
+    taskRef.current.open();
+  }
+
+  function handleOpenWorkspace() {
+    workspaceRef.current.open();
+  }
+
   return (
     <>
+      <AddingProject ref={projectRef} />
+      <AddingTask ref={taskRef} />
+      <AddingWorkspace ref={workspaceRef} />
       <header className={classes.header}>
         <div className={classes.logoContainer}>
           <Link to={`/${currentUser}/home`}>
@@ -87,9 +115,26 @@ export default function HeadMenu() {
               </Link>
             </li>
             <li className={classes.li}>
-              <Link to="#" className={classes.button}>
+              <button onClick={toggleCreateMenu} className={classes.button}>
                 Utwórz
-              </Link>
+              </button>
+              {isCreateMenuOpen && (
+                <div
+                  style={{
+                    backgroundColor: "white",
+                    boxShadow: "0px 8px 16px rgba(0,0,0,0.2)",
+                    zIndex: 1,
+                    borderRadius: "1rem",
+                    border: "none",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <button onClick={handleOpenWorkspace}>Workspace</button>
+                  <button onClick={handleOpenProject}>Project</button>
+                  <button onClick={handleOpenTask}>Activity</button>
+                </div>
+              )}
             </li>
           </ul>
         </menu>
